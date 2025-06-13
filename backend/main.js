0f7d0d7ce78429e8a9672119a8c0923bfe0aab68
jegen <jean-ely.gendrau@laplateforme.io>
2025-06-13 14:38:06 +0200

    chore: initialize backend with package.json and essential dependencies

---
+++ backend/main.js
@@ -0,0 +1,35 @@
+import express from "express"
+import mysql from 'mysql2/promise';
+
+const apiRest = express();
+
+const PORT = process.env.PORT || 3000;
+
+const connection = await mysql.createConnection({
+  host: 'bd',
+  user: 'admin-user',
+  password: 'admin-user',
+  database: 'node_product',
+  port: 3306
+});
+
+
+
+apiRest.get("/", async (req, res) => {
+  try {
+    // execute will internally call prepare and query
+    const [results, fields] = await connection.execute(
+      'SELECT * FROM `products`'
+    );
+
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
+
+
+});
+
+apiRest.listen(PORT, "0.0.0.0", () => {
+  console.log("Server start on http://localhost:" + PORT);
+})
